93e68fef67cb87944745bd8c47387bf23e07ef7c
damonwch <41980443+damonwch@users.noreply.github.com>
2024-04-08 19:23:08 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,14 +1,25 @@
-from flask import Flask, request, jsonify
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
+import json
+import time
 
-app = Flask(__name__)
+app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_credentials=True,
+                   allow_origins=["*"],
+                   allow_methods=["*"],
+                   allow_headers=["*"]
+                   )
 
-@app.route('/gemini/chat', methods=['POST'])
-def gemini_chat():
-    data = request.get_json()
+
+@app.post("/gemini/chat", tags=["API"], summary="GEMINI")
+def gemini_chat(data: dict):
     prompt = data.get('prompt')
     api_key= data.get('api_key')
     try:
+        # genai.configure(api_key=api_key,transport='rest')
         genai.configure(api_key=api_key)
         model = genai.GenerativeModel('gemini-pro')
         chat = model.start_chat(history=[])
@@ -16,10 +27,9 @@ def gemini_chat():
         text=response.text
         response = {"content": text}
         return response
-    
     except Exception as e:
-        print(e)
+        print("gemini_chat:",e)
         return None
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1",workers=1)
