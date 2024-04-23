9235cd1b632caca5cb98af5b9f069d387d10fa5a
damonwch <41980443+damonwch@users.noreply.github.com>
2024-04-23 14:38:40 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -18,18 +18,24 @@ app.add_middleware(CORSMiddleware,
 def gemini_chat(data: dict):
     prompt = data.get('prompt')
     api_key= data.get('api_key')
+    history=data.get('history')
+    if history is None:
+        history=[]  
     try:
         # genai.configure(api_key=api_key,transport='rest')
         genai.configure(api_key=api_key)
         model = genai.GenerativeModel('gemini-pro')
-        chat = model.start_chat(history=[])
+        chat = model.start_chat(history=history)
         response = chat.send_message(prompt)
         text=response.text
         response = {"content": text}
+        # for message in chat.history:
+        #     print(f'**{message.role}**')
         return response
     except Exception as e:
         print("gemini_chat:",e)
         return None
 
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1",workers=1)
